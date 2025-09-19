4094e7d8b5cc052653633f74633e4b7d9a588aee
Potatomine725 <WotblitzGamer725@gmail.com>
2025-09-19 16:55:37 +0700

    append

--- Bai6/src/Bai6.java
+++ Bai6/src/Bai6.java
@@ -1,19 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+import java.util.Scanner;
 
-/**
- *
- * @author Wotbl
- */
 public class Bai6 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+		KhachSan ql111 = new KhachSan();
+
+		ql111.nhapDanhSach(sc);
+
+		System.out.print("Nhap vao khach tro can tinh tien: ");
+		int cmnd = sc.nextInt();
+
+		System.out.println("==> Tong tien la: " + ql111.tinhTien(cmnd));
+
+		sc.close();
     }
     
 }
